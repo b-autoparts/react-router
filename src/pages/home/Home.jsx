b08cccedd2742650bc26a69fc90f13ede1697d8f
kittisak <kboonpinyo@gmail.com>
2021-10-15 08:34:26 +0700

    update homework

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,20 @@
+import {
+    MDBCard,
+   
+  } from "mdb-react-ui-kit";
 export function Home(){
     return (
         <div>
-            <h1>this is home page</h1>
+            <h3>this is home page</h3>
+            <br />
+            <br />
+            {/* <img src="/images/bae.png" alt="" /> */}
+            {/* <img src="/images/logo.png" /> */}
+            <div className="cards">
+        <MDBCard alignment="center" style={{ maxWidth: "500px" }}>
+        <img src="/images/bae.png" alt="" />
+        </MDBCard>
+      </div>
         </div>
 
     )
